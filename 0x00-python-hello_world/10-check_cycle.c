04f634b2adda6fc56b883740b6605fcd9bf6314b
shehapkhalil62@gmail.com <shehapkhalil62@gamil.com>
2023-11-27 04:37:17 -0800

    edit

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * check_cycle - check cycle
+ * @list:header
+ * Return:0 or 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list, *fast = list;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
