742c2f11d069386bd917c027f9b853985c834ea0
shehapkhalil62@gmail.com <shehapkhalil62@gamil.com>
2023-11-28 03:49:41 -0800

    edit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i == 99:
-        print(i)
-    else:
-        print("{:0>2d}".format(i), end=", ")
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        if i == 8:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
