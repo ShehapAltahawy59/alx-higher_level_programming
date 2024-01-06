9125297ada903ebfa430510e9f39d2c8eafaaffc
shehap altahawy <shehapkhalil62@gmail.com>
2024-01-06 14:35:19 -0800

    edit

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    x=my_list.copy()
+    x[idx]=element
+    return x
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    # If a tuple is smaller than 2, use the value 0 for each missing integer
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    
+    # If a tuple is bigger than 2, use only the first 2 integers
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    
+    return result
