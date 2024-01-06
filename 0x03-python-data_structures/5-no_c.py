4df8ba5dcab403776670307d8e0b5dd765274638
shehap altahawy <shehapkhalil62@gmail.com>
2024-01-06 14:44:44 -0800

    edit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,3 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_string=""
-    for x in my_string:
-        if(x == "c" or x == "C"):
-            pass
-        else:
-            new_string+=x
-    return new_string
+    return ''.join(char for char in my_string if char not in 'cC')
