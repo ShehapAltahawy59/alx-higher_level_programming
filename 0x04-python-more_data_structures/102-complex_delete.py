ebab039c4c6f04eda761366f30f0fd154f85753e
shehap altahawy <shehapkhalil62@gmail.com>
2024-01-10 14:16:02 -0800

    edit

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    return {k: v for k, v in a_dictionary.items() if v != value}
+    a_dictionary =  {k: v for k, v in a_dictionary.items() if v != value}
+    return a_dictionary
