087e778e41e1377db22a10e17da96905cf525d25
shehap altahawy <shehapkhalil62@gmail.com>
2024-01-10 12:35:38 -0800

    edit

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if (len(matrix > 0)):
+        new_list=[]
+        for i in matrix:
+            newlist.append(list(map(lambda x : x**2,i)))
+            return new_list
+                
