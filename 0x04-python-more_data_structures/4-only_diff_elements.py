0eb1f228fd70c2fe2ed6f67a4445de130f937936
shehap altahawy <shehapkhalil62@gmail.com>
2024-01-10 13:20:15 -0800

    edit

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    set_3 = set_1.intersection(set_2)
-    return set(x for x in set_1.union(set_2) if x not in set_3)
+def diff_elements(set_1, set_2):
+    set3 = set_1 & set_2
+    return set_1 ^ set_2
