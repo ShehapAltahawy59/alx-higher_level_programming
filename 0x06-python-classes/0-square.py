1ac4ba5efa79bfb844f91e966f86a40bf2d7ecee
ShehapAltahawy59 <142473131+ShehapAltahawy59@users.noreply.github.com>
2024-01-23 09:42:56 +0200

    Add files via upload

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+this is the "Square" module.
+
+it provides a empty Square class.
+"""
+
+
+class Square:
+    """A Square class"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+this is a "Square" module.
+
+it provides a Square class with initializtion.
+"""
+
+
+class Square:
+    """A Square class with initialization size"""
+    def __init__(self, size):
+        self.__size = size
