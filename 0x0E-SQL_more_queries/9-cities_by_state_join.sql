fcdd360c2d99a92e537433799eeee0b06e1e5f6a
shehap62 <shehapkhalil62@gmail.com>
2024-03-13 12:58:31 +0200

    edit

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,1 +1,7 @@
-SELECT id name FROM cities INNER JOIN states ON cities.id = state_id ORDER BY cities.id;
+-- Lists all cities in the database hbtn_0d_usa.
+-- Records are sorted in order of ascending cities.id.
+SELECT id name 
+FROM cities 
+INNER JOIN states 
+ON cities.id = state_id 
+ORDER BY cities.id;
