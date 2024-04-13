c42168bf3f884cd57deeaa025051afe430c888ba
shehap62 <shehapkhalil62@gmail.com>
2024-04-13 20:34:14 +0200

    edit

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     Base.metadata.bind = engine
     session = Session(engine)
     state = session.query(State)\
-                        .filter(State.name == (sys.argv[4]))\
-                        .order_by(State.id).first()
+    .filter(State.name == (sys.argv[4]))\
+    .order_by(State.id).first()
     if state:
         print("{}".format(state.id))
     else:
