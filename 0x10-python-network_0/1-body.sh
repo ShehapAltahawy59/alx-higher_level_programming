ef2ef4dde81d8d9273f51abb08419c13922ca617
shehap62 <shehapkhalil62@gmail.com>
2024-04-25 16:35:57 +0200

    edit

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Get the byte size of the HTTP response header for a given URL.
+#  takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -sL "$1"
