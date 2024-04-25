2d3305ad022201866cc949e040eb10883345cf6b
shehap62 <shehapkhalil62@gmail.com>
2024-04-25 16:41:15 +0200

    edit

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# Check if URL has been provided
+#  takes in a URL, sends a GET request to the URL, and displays the body of the response
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <url>"
     exit 1
 fi
-
-# Send DELETE request and display body of the response
 curl -s -X DELETE "$1"
