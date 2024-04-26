25b5bec1b4e7ef0b32f113d00886161ff8b13c01
shehap62 <shehapkhalil62@gmail.com>
2024-04-26 22:31:55 +0300

    edit

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -10,5 +10,5 @@ import sys
 
 if __name__ == "__main__":
     payload = {'email': sys.argv[2]}
-    r = requests.post(sys.argv[1],data=payload)
+    r = requests.post(sys.argv[1], data=payload)
     print(r.text)
