aaa7503ca25ed2b771c6657ec21ec1da888c34ac
shehap62 <shehapkhalil62@gmail.com>
2024-04-26 22:56:53 +0300

    edit

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """A script that:
-- takes in a URL
-- sends a POST request to the passed URL
-- takes email as a parameter
-- displays the body of the response
+- takes your GitHub credentials (username and password)
+- uses the GitHub API to display your id
 """
-import requests
 import sys
+import requests
 from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     r = requests.get("https://api.github.com/user", auth=auth)
-    print(r)
+    print(r.json().get("id"))
