b36ca6a18ef952e7d0671a8221d0b15e318c3371
shehap62 <shehapkhalil62@gmail.com>
2024-04-26 22:05:18 +0300

    edit

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""A script that:
+- takes in a URL
+- sends a POST request to the passed URL
+- takes email as a parameter
+- displays the body of the response
+"""
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    request = urllib.request.Request(url)
+    try: 
+        urllib.request.urlopen(request)
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("utf-8"))
+    except urllib.error.URLError as e:
+        print(e.reason)    
+    
+    
