4d55d5abf443fb12c7f1ab39f2518d0934de2ebf
shehap62 <shehapkhalil62@gmail.com>
2024-04-26 22:34:43 +0300

    edit

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A script that:
+- takes in a URL
+- sends a POST request to the passed URL
+- takes email as a parameter
+- displays the body of the response
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
+    
