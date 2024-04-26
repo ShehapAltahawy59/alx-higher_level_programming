b0cbe062dbed664156e6bb98bae72da41fa8bd1d
shehap62 <shehapkhalil62@gmail.com>
2024-04-26 21:43:09 +0300

    edit

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# fetches https://alx-intranet.hbtn.io/status
+import urllib.request
+import sys
+
+with urllib.request.urlopen(sys.argv[1]) as response:
+    data = response.info()
+    x_request_id = data.get("X-Request-Id")
+    print(x_request_id)
