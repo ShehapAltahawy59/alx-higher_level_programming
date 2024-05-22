7b7804122623774526a0f1d53b9f9616873b798a
shehap62 <shehapkhalil62@gmail.com>
2024-05-22 11:50:47 +0300

    edit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
-// Reads file from args and displays contents
-
-const filePath = process.argv[2];
 const fs = require('fs');
-
-fs.readFile(filePath, 'utf-8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data.toString());
-  }
-});
+const file = process.argv[2];
+try {
+  const data = fs.readFileSync(file, 'utf8');
+  console.log(data);
+} catch (err) {
+  console.error(err);
+}
